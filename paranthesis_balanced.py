599262f5221805f4f1087eb9729f35105bf6dd3a
Somatich <44047572+Somatich@users.noreply.github.com>
2019-05-22 12:37:50 +0700

    alg 6 lab

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -1,6 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from stack import Stack, StackOverflowError, StackIsEmptyError
+s = '({[()]}{<>})'
 def is_paranthesis_balanced(s):
-    pass
+    a = list(s)
+    parat = Stack(len(a))
+    for i in a:
+        print(i)
+        try:
+            if i == '(':
+                parat.push('(')
+            elif i == ')':
+                if parat.pop() != '(':
+                    print('круглые скобочки не закрыты')
+                    return False
+            elif i == '[':
+                parat.push('[')
+            elif i == ']':
+                if parat.pop() != '[':
+                    print('квадратные скобочки не закрыты')
+                    return False
+            elif i == '{':
+                parat.push('{')
+            elif i == '}':
+                if parat.pop() != '{':
+                    print('фигурные скобочки не закрыты')
+                    return False
+            elif i == '<':
+                parat.push('<')
+            elif i == '>':
+                if parat.pop() != '<':
+                    print('треугольные скобочки не закрыты')
+                    return False
+        except StackOverflowError:
+            print('лишняя закрывающая скобочка DETECTED')
+            return False
+        except StackIsEmptyError:
+            print('лишняя открывающая скобочка DETECTED')
+            return False
+    if parat.__len__() == 0:
+        print('выражение кайфовое')
+        return True
+    else:
+        return False
+print(is_paranthesis_balanced(s))
 
+#is_paranthesis_balanced(s = '({[()]}{<>})')
--- stack.py
+++ stack.py
@@ -14,12 +14,31 @@ class Stack:
     def __init__(self, size):
         self.storage = [0] * size
         self.head = -1
+        self.size = size
 
     def push(self, v):
-        pass
-
+        if self.head < self.size-1:
+            self.storage[self.head + 1] = v
+            self.head += 1
+            return
+        else:
+            raise StackOverflowError
+        
     def pop(self):
-        pass
+        if self.head > -1:
+            b0x = self.storage[self.head]
+            self.storage[self.head] = 0
+            self.head -= 1
+            return b0x
+        else:
+            raise StackIsEmptyError
 
     def __len__(self):
-        pass
+        return self.head + 1
+
+#
+#s = [1,2,3,4,5,6,7]
+#i = Stack(1000)
+#for l in s:
+#    i.push(l)
+#    print(i.pop())
